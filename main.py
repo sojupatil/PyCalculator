8183b9b96b47ba1648f5f738accd9f93bc4b3627
Sojwal Patil <107755554+sojupatil@users.noreply.github.com>
2025-07-04 17:04:45 +0530

    Create main.py
    A simple command-line calculator built with Python that performs basic arithmetic operations: addition, subtraction, multiplication, and division. Includes error handling for invalid inputs like division by zero. Ideal for beginners learning how to take user input, use functions, and apply basic control flow.

---
+++ main.py
@@ -0,0 +1,29 @@
+def add(n,m):
+    return n+m
+
+def sub(n,m):
+    return n-m
+
+def multiply(n,m):
+    return n*m
+
+def divide(n,m):
+    if m==0:
+        return "Error : You cannot divide by Zero"
+    return n/m
+
+
+first = int(input("Enter First Number "))
+second = int(input("Enter Second Number "))
+option = input("Enter Choice:\n\nEnter add for Addition\nEnter sub for Subtraction\nEnter mul for Multiplication\nEnter div for Divide\n")
+
+if(option == "add"):
+    print(add(first,second))
+elif (option == "sub"):
+    print(sub(first,second))
+elif (option == "mul"):
+    print(multiply(first,second))
+elif (option == "div"):
+    print(divide(first,second))
+else:
+    print("Error : Invalid") 
